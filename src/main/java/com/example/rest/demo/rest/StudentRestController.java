f20b43a40a514671fffb77f83879a4f355265511
hackgone <bishtpiyush17@gmail.com>
2023-07-19 17:49:02 +0530

    added postconstruct

--- src/main/java/com/example/rest/demo/rest/StudentRestController.java
+++ src/main/java/com/example/rest/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.example.rest.demo.rest;
 
 import com.example.rest.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,19 @@ import java.util.*;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    //define endpoint for /students return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudent = new ArrayList<>();
+    private List<Student> theStudent;
+
+    //postconstruct will be called once after bean is loaded
+    @PostConstruct
+    public void loadData(){
+        theStudent = new ArrayList<>();
         theStudent.add(new Student("Piyush","Bist"));
         theStudent.add(new Student("Piyush",""));
         theStudent.add(new Student("Piyush","hell"));
+    }
+    //define endpoint for /students return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return theStudent;
     }
 }
